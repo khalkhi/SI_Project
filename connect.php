832702271ada1d626c550c85618e5f7eb7e2ae93
khaleel white <khaleelwhite10@gmail.com>
2024-11-06 00:20:34 -0500

    edits to connect

--- connect.php
+++ connect.php
@@ -10,7 +10,10 @@
     $conn = new mysqli($servername, $username, $password, $db_name);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    //to close the connection
+//$conn->close();
 ?>
